0c37607d1f18e693e31f3534fc42292317284d77
David <136182039+david0154@users.noreply.github.com>
2025-06-14 18:33:43 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Replace with your Firebase project config
+const firebaseConfig = {
+  apiKey: "AIzaSyB9SGeSzQSP63szOYyzEFCd9-f26KRNA5I",
+  authDomain: "dk-chats-2919c.firebaseapp.com",
+  databaseURL: "https://dk-chats-2919c-default-rtdb.firebaseio.com",
+  projectId: "dk-chats-2919c",
+  storageBucket: "dk-chats-2919c.appspot.com",
+  messagingSenderId: "620357656811",
+  appId: "1:620357656811:web:ee4ada3c2e8baa0d679eae",
+  measurementId: "G-XHS1E7PRKL"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
+const auth = firebase.auth();
